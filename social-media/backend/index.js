75dbcc90f6febb741a8ddeae04c7aece7c8d8c3b
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2023-05-07 23:47:35 +0530

    Added file upload api in backend.
    Added file upload api in backend.

--- social-media/backend/index.js
+++ social-media/backend/index.js
@@ -1,40 +1,58 @@
 const express = require("express");
-const mongoose =require("mongoose");
-const helmet= require("helmet");
-const morgan =require("morgan");
-const dotenv =require("dotenv");
-const userRoute =require("./routes/users");
-const authRoute =require("./routes/auth");
-const postRoute =require("./routes/posts");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
-const app =express();
-dotenv.config()
+const app = express();
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', function() {
-  console.log('Connected to MongoDB');
+db.once("open", function () {
+  console.log("Connected to MongoDB");
 });
 
 console.log(`Mongoose connection state: ${db.readyState}`);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-//middleware 
+//middleware
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", userRoute);
-app.use("/api/auth",authRoute);
-app.use("/api/posts",postRoute)
-// app.get("/", (req,res)=>{
-//     res.send('welcome to Home Page')
-// }) 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-app.listen(8800,()=>{
-    console.log(" Backend is running")
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log(" Backend is running");
+});
